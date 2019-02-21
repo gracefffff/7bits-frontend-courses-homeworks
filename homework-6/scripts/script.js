868f13d13a3b02488b5fbd88356056608501a43a
Mihail Saytiev <saitiev2008@gmail.com>
2019-02-21 21:59:37 +0600

    done homework6

---
+++ homework-6/scripts/script.js
@@ -0,0 +1,20 @@
+var parameters = new URLSearchParams(location.search);
+var query_parameters = {};
+parameters.forEach((value, key) => query_parameters[key] = value );
+console.log(query_parameters);
+if (query_parameters['firstName'] !== undefined) {
+    document.getElementById("first-name").value = query_parameters['firstName'];
+}
+if (query_parameters['lastName'] !== undefined) {
+    document.getElementById("last-name").value = query_parameters['lastName'];
+}
+if (query_parameters['email'] !== undefined) {
+    document.getElementById("email").value = query_parameters['email'];
+}
+if(query_parameters['gender'] === 'male') {
+    document.getElementById('gender-male').checked = true;}
+    else if (query_parameters['gender'] === 'female') {
+    document.getElementById('gender-female').checked = true;
+}
+
+
